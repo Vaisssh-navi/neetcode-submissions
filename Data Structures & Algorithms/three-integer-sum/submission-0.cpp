2112952e2ce9c799974defe0e76e2203d70713ff
Vaishnavi <ee1221719@iitd.ac.in>
2026-07-04 11:47:46 +0530

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+// brutre force - n^3logn 
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int left=i+1;
+            int right=n-1;
+            while(left<right){
+                int sum=nums[left]+nums[right]+nums[i];
+                if(sum==0){
+                 ans.push_back({nums[left],nums[right],nums[i]});
+                 left++;
+                 right--;
+                 while(left<right && nums[left]==nums[left-1]){
+                    left++;
+                 }
+                 while(left<right && nums[right]==nums[right+1]){
+                    right--;
+                 }
+                }else if(sum>0){
+                    right--;
+
+                }else{
+                    left++;
+                }
+
+            }
+        }
+        return ans;
+
+    }
+};
